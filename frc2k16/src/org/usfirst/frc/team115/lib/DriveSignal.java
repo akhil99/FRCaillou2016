ab8169fae672996327b769a1b6ac34e8b6170114
Ishan Mitra <ishan22@users.noreply.github.com>
2016-05-09 09:52:49 -0700

    Add DriveSignal.java

---
+++ frc2k16/src/org/usfirst/frc/team115/lib/DriveSignal.java
@@ -0,0 +1,12 @@
+
+public class DriveSignal {
+    public double leftMotor;
+    public double rightMotor;
+
+    public DriveSignal(double left, double right) {
+        this.leftMotor = left;
+        this.rightMotor = right;
+    }
+
+    public static DriveSignal NEUTRAL = new DriveSignal(0, 0);
+}
